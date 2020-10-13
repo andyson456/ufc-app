6e3ee8b8aad0b7cdb36cd1a1e14d09cdecc2a5a4
Andrew Larson <andyson456@gmail.com>
2020-10-13 15:03:26 -0700

    Recent commit

--- ufcapp/src/actions.js
+++ ufcapp/src/actions.js
@@ -9,12 +9,12 @@ export function mapStateToProps(state) {
 export function mapDispatchToProps(dispatch) {
     return {
         loadCompetitions: function (){
-        fetch(`http://localhost:3333/api/competitions/`)
+        fetch(`/api/competitions/`)
         .then((res) => res.json())
         .then((data) => dispatch({type: "LOAD_COMPETITIONS", competitions: data.competitions}))
         },
         loadRankings: function (){
-        fetch(`http://localhost:3333/api/rankings/`)
+        fetch(`/api/rankings/`)
         .then((res) => res.json())
         .then((data) => dispatch({type: "LOAD_RANKINGS", rankings: data.rankings}))
         },
